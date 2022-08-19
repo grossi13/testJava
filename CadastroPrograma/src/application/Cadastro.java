3d1b999fc5396c665eb519931401ef2d83df08db
Vinicius Grossi <80795882+grossi13@users.noreply.github.com>
2022-08-18 23:42:58 -0300

    Update Cadastro.java

--- CadastroPrograma/src/application/Cadastro.java
+++ CadastroPrograma/src/application/Cadastro.java
@@ -4,9 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
-
 import entities.Employee;
-import java.util.Locale;
 
 public class Cadastro {
 	public static void main(String[] args) {
